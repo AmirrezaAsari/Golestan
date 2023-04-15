16fc6be21437fea085ac13b36d34abff856ccaa6
unknown <amirreza.asari2@gmail.com>
2023-04-15 22:03:16 +0430

    admin completed

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@ struct user{
     string password;
     user *nxt;
 };
+struct lesson{
+	string class_name;
+	string calss_id;
+	string teacher_fname;
+	string teacher_lname;
+	string teacher_id;
+	//user *student;
+	lesson *nxt;
+};
+
 
 user* login();
 void admin();
@@ -78,26 +88,8 @@ user* login(){
 	return NULL;
 }
 
-void admin(){
-	int toDo = -1;
-	while(toDo != 0){
-		cout<<"___________________ADMIN___________________\n";
-		cout<<"Please Enter what do you want to do? \n";
-		cout<<"1.New Class \n2.Edit users\n0.exit\n";
-		cin>>toDo;
-		switch(toDo){
-			case 1:
-				//new_class();
-				break;
-			case 2:
-				edit_users();
-				break;
-			case 0:
-				break;
-		}
-	}
-}
 
+	
 void edit_users(){
 	ifstream users("Users.txt");
     string role;
@@ -155,6 +147,100 @@ void edit_users(){
         head = curr;
     }
     head = NULL;
+}
+
+void new_class(){
+    lesson *Head = NULL;
+    lesson *curr;
+
+    ifstream Classes("Classes.txt");
+    string class_name;
+    string class_id;
+    while(Classes >> class_name >>class_id){
+        curr = new lesson;
+        curr->nxt = Head;
+        Head = curr;
+    }
+    
+    curr = new lesson;
+    cout<<"Enter Calss Name : ";
+    cin>>curr->class_name;
+    cout<<"Enter Class ID: ";
+    cin>>curr->calss_id;
+    ifstream users("Users.txt");
+    string role;
+    string first_name;
+    string last_name;
+    string username;
+    string password;
+	user *tmp;
+	user *head = NULL;
+    while(users >> role >>  first_name >> last_name >> username >> password){
+    	tmp = new user;
+        tmp->role = role;
+        tmp->first_name = first_name;
+        tmp->last_name = last_name;
+        tmp->username = username;
+        tmp->password = password;
+        tmp->nxt = head;
+        head = tmp;
+	}
+	int i=1;
+	while(tmp != NULL){
+		if(tmp->role == "1"){
+			cout<<i<<"."<<tmp->username<<"\t"<<tmp->first_name<<"\t"<<tmp->last_name<<endl;
+			i++;
+		}
+		tmp = tmp->nxt;
+	}
+	cout<<"Enter Class teacher's username: ";
+	cin>>curr->teacher_id;
+	cout<<"Enter Class teacher's first name: ";
+	cin>>curr->teacher_fname;
+	cout<<"Enter Class teacher's last name: ";
+	cin>>curr->teacher_lname;
+    curr->nxt = Head;
+    Head = curr;
+
+    cout<<"Class Added.\n";
+    cout<<"Class Name : "<<curr->class_name<<"\nClass ID : "<<curr->calss_id<<endl;
+
+
+
+    curr = Head;
+    ofstream out_class("Classes.txt");
+    while(curr!= NULL){
+        out_class<<curr->class_name<<" "<<curr->calss_id;
+        curr = curr->nxt;
+        if(curr!= NULL)
+            out_class<<endl;
+    }
+
+    while(Head!= NULL){
+        curr = Head->nxt;
+        delete Head;
+        Head = curr;
+    }
+}
+
+void admin(){
+	int toDo = -1;
+	while(toDo != 0){
+		cout<<"___________________ADMIN___________________\n";
+		cout<<"Please Enter what do you want to do? \n";
+		cout<<"1.New Class \n2.Edit users\n0.exit\n";
+		cin>>toDo;
+		switch(toDo){
+			case 1:
+				new_class();
+				break;
+			case 2:
+				edit_users();
+				break;
+			case 0:
+				break;
+		}
+	}
 }
 
 
